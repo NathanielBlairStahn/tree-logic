e761ad03302071c55fcbac4abe0c647e76ff02c8
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2018-01-25 12:08:42 -0800

    random changes to web app

--- web/trees_app.py
+++ web/trees_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 import io
 from PIL import Image
 from model import ImageClassifier
@@ -13,10 +13,13 @@ def index():
 
 @app.route('/identify', methods=['POST'])
 def identify():
-    image = request.files['image'].read()
-    img_bytes = io.BytesIO(image)
-    predictions = classifier.predict(image)
-    return predictions
+    file_object = request.files['image']
+    image = file_object.read()
+    print(type(image))
+    predictions = image
+    # img_bytes = io.BytesIO(image)
+    # predictions = classifier.predict(image)
+    return Response(response=predictions, status=200, mimetype="text/plain")
 
 if __name__ == '__main__':
     #threaded=True creates separate thread for each person who visits website
