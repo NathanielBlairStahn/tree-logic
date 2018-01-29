f1ee83aa81ccf8d7f5b2c592654409fe6d756d72
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2018-01-29 11:59:24 -0800

    add directory specification to scrape_trees.ipynb, cite space removal script

--- src/remove_filename_spaces.sh
+++ src/remove_filename_spaces.sh
@@ -1,4 +1,6 @@
-#Replaces spaces with underscores in filenames
+#Replaces spaces with underscores in filenames, thanks to Stack Exchange:
+#https://stackoverflow.com/questions/11752878/awk-sed-one-liner-command-for-removing-spaces-from-all-file-names-in-a-given
+#https://unix.stackexchange.com/questions/223182/how-to-replace-spaces-in-all-file-names-with-underscore-in-linux-using-shell-scr
 for name in *\ *; do mv -v "$name" "${name// /_}"; done
 
 #Example:
