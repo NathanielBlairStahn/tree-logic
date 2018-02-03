0137e4360a7003b24cd2936f77e149220022c3b2
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2018-02-03 15:04:20 -0800

    start writing ImageManager class, add scratchwrk notebook

--- src/image_manager.py
+++ src/image_manager.py
@@ -6,9 +6,70 @@ import os
 from PIL import Image
 import imagehash
 
+from collections import defaultdict
+
 class ImageManager():
     def __init__(self):
         self.image_extensions = ['.png', '.jpg', '.jpeg']
+        self.base_directory = 'tree_photos'
+        self.hash_fcn = imagehash.phash
+
+        self.label_col = 'species'
+        self.file_col = 'filename'
+        self.hash_col = 'p_hash'
+
+        self.image_dict = defaultdict(list)
+
+    def image_hashes_to_paths(directory,
+                              image_dict = defaultdict(list),
+                              hash_fcn = imagehash.phash):
+        '''
+        INPUTS:
+            directory (string) - name of the directory containing image files to hash.
+            image_dict (ddefaultdict(list)) - a dictionary mapping previously found
+            image hashes to a list of their paths. If no dictionary is given, a new
+            one is created.
+
+        RETURNS: The updated default dictionary mapping hash values of images in the directory
+        to a list of image paths with the same hash.
+        '''
+        # # Dictionary to map hash values to image files
+        # image_dict = defaultdict(list)
+
+        #Loop through all images in the directory, and make lists of
+        #images that have the same hash
+        for image_filename in os.listdir(directory):
+            image_path = os.path.join(directory, image_filename)
+            try:
+                img = Image.open(image_path)
+                #Use 'perception hash' to compute the hash of an image: http://www.phash.org/
+                #phash  uses the Discrete Cosine Transform to convert images into
+                #frequency space
+                h = str(hash_fcn(img))
+                image_dict[h].append(image_path)
+            except OSError:
+                print("Cannot open file: {}".format(image_path))
+
+        return image_dict
+
+    def remove_duplicate_images(image_dict):
+        '''
+        Removes duplicate images found in image_dict. All duplicate
+        images after the first occurrence in the list of repeats are removed.
+
+        INPUT: image_dict - dictionary of image paths to remove images from.
+        The key is the image's hash vlaue
+        OUTPUT: None
+        '''
+        # # Dictionary to map hash values to image files
+        # image_dict = create_image_dict(directory)
+
+
+        #Find all hashes that had more than one image, and remove duplicates
+        for hash_val, image_paths in image_dict.items():
+            if len(image_paths) > 1:
+                for image_path in image_paths[1:]:
+                    os.remove(image_path)
 
 
     def add_images_to_df(self,
