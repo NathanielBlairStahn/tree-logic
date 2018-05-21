b6b11c403a46c3cebf78816bc2c4ae6e90454bea
Nathaniel Blair-Stahn <nathaniel.blair.stahn@gmail.com>
2018-05-21 12:39:51 -0700

    add 7 species confusion, ranked prediction

--- src/model_eval.py
+++ src/model_eval.py
@@ -1,13 +1,58 @@
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import itertools
+
 #from sklearn.model_selection import train_test_split
 from sklearn.metrics import log_loss, accuracy_score, confusion_matrix, matthews_corrcoef
+
+def top_k_predictions(y_true, y_pred_probs, classes, k=3):
+    return np.array(classes[np.argsort(y_pred_probs, axis=1)[:,:-k-1:-1]])
 
 def top_k_accuracy(y_true, y_pred_probs, classes, k=3):
     top_k_predictions = classes[np.argsort(y_pred_probs, axis=1)[:,-k:]]
     #print(ranked_predictions)
     return np.array([y_true.iloc[i] in top_k_predictions[i] for i in range(len(y_true))]).mean()
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion Matrix\n(Number of images by class)',
+                          cmap=plt.cm.Blues,
+                          fontsize=22,
+                         colorbar=True):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = np.round(cm/ cm.sum(axis=1)[:, np.newaxis],2)
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    if colorbar:
+        plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    #formatter = FuncFormatter(to_percent)
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j],fmt),
+                 fontdict={'size': fontsize, 'weight': 'bold'},
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 class ModelEvaluator():
     def __init__(self, model): #, image_df):
